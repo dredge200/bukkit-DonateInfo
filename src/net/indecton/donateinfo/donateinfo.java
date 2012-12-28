5795611c4ec9771beb17cd0e128a633eb58db041
Corvin Mcpherson <corvin455@gmail.com>
2012-12-28 16:24:46 -0500

    Added onCommand()

--- src/net/indecton/donateinfo/donateinfo.java
+++ src/net/indecton/donateinfo/donateinfo.java
@@ -1,8 +1,14 @@
 package net.indecton.donateinfo;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class donateinfo extends JavaPlugin
 {
-	
+	public boolean onCommand(CommandSender sender, Command cmd, String cmdlabel, String[] args)
+	{
+		
+		return true;
+	}
 }
