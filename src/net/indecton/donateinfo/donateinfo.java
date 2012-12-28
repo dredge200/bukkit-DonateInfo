e086dfd9b0fcedd3fd21f2c9084666b6d5223ece
Corvin Mcpherson <corvin455@gmail.com>
2012-12-28 16:57:46 -0500

    Added server log for config reload

--- src/net/indecton/donateinfo/donateinfo.java
+++ src/net/indecton/donateinfo/donateinfo.java
@@ -37,6 +37,7 @@ public class donateinfo extends JavaPlugin
 					reloadConfig();
 					loadConfig();
 					sender.sendMessage(ChatColor.GREEN + "Config Reloaded.");
+					getServer().getLogger().info(sender.getName() + " reloaded the config.");
 				}
 			}
 		}
