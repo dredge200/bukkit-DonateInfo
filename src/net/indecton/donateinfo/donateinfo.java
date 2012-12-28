147d4430076fc254c7e7574829f1e100c08114f0
Corvin Mcpherson <corvin455@gmail.com>
2012-12-28 16:44:04 -0500

    Added loadConfig() function

--- src/net/indecton/donateinfo/donateinfo.java
+++ src/net/indecton/donateinfo/donateinfo.java
@@ -1,6 +1,7 @@
 package net.indecton.donateinfo;
 
 import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -8,6 +9,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class donateinfo extends JavaPlugin
 {
 	private List<String> msg;
+	
+	private void loadConfig()
+	{
+	}
 	public boolean onCommand(CommandSender sender, Command cmd, String cmdlabel, String[] args)
 	{
 		
