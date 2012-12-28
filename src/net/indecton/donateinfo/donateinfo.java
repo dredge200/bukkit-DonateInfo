5d79c3644a08b713a90602715e940e3f13eac314
Corvin Mcpherson <corvin455@gmail.com>
2012-12-28 16:43:05 -0500

    Added local variable (List<String>) msg

--- src/net/indecton/donateinfo/donateinfo.java
+++ src/net/indecton/donateinfo/donateinfo.java
@@ -1,11 +1,13 @@
 package net.indecton.donateinfo;
 
+import java.util.List;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class donateinfo extends JavaPlugin
 {
+	private List<String> msg;
 	public boolean onCommand(CommandSender sender, Command cmd, String cmdlabel, String[] args)
 	{
 		
