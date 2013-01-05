45d6e0d4be910c6cf0db31f44a608dc7fa13a933
Corvin Mcpherson <corvin455@gmail.com>
2013-01-04 21:44:25 -0500

    Added formatColors(String) function

--- src/net/indecton/donateinfo/donateinfo.java
+++ src/net/indecton/donateinfo/donateinfo.java
@@ -12,6 +12,39 @@ public class donateinfo extends JavaPlugin
 {
 	private List<String> msg;
 	
+	private String formatColors(String s)
+	{
+		String tmp = "";
+		for (int i=0; i < s.length(); i++) //For each character in String s
+		{
+			if (s.charAt(i) == '$') //If character is $
+			{
+				if (i+1 < s.length()) //If is not last character
+				{
+					i++;
+					if ((s.charAt(i) >= '0' && s.charAt(i) <= '9') ||
+							(s.charAt(i) >= 'a' && s.charAt(i) <= 'f') ||
+							(s.charAt(i) >= 'k' && s.charAt(i) <= 'o') ||
+							(s.charAt(i) == 'r') || (s.charAt(i) == '_')) //Is valid symbol/color/format code
+					{
+						if (s.charAt(i) == '_') //Is symbol code
+						{
+							tmp += '$';
+						}
+						else //Is color/format code
+						{
+							tmp += ChatColor.COLOR_CHAR;
+							tmp += s.charAt(i);
+						}
+					}
+			else //If character is not $
+			{
+				tmp += s.charAt(i);
+			}
+		}
+		return tmp;
+	}
+	
 	private void loadConfig()
 	{
 		if (new File("plugins/donateinfo/config.yml").exists() == false)
